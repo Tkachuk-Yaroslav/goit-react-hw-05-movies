9f60e24beb455b755fada8da57fa30f36715c050
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-26 14:31:36 +0300

    Added links to Cast and Reviews components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,9 @@ import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MoviesDetails from 'pages/MoviesDetails';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './Layout';
+import Layout from './Layout/Layout';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
 
 const App = () => {
   return (
@@ -11,7 +13,10 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MoviesDetails />} />
+          <Route path="movies/:movieId" element={<MoviesDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </div>
--- src/pages/MoviesDetails.jsx
+++ src/pages/MoviesDetails.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 const MoviesDetails = () => {
   const params = useParams();
   console.log('params', params);
-  return <div>MoviesDetails MoviesDetails MoviesDetails </div>;
+  return (
+    <>
+      <div>MoviesDetails - {params.movieId} </div>
+      <Link to={'cast'}>Cast</Link>
+      <Link to={'reviews'}>Reviews </Link>
+      <Outlet />
+    </>
+  );
 };
 
 export default MoviesDetails;
